0c25879986220d3c1a34db01051ca5b285aa470f
a6ram4uk <a6ram4uk@mail.ru>
2016-07-06 17:54:07 +0300

    Add object Cube + script for rotate

---
+++ Assets/Scripts/CubeFloat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeFloat : MonoBehaviour 
+{
+
+	public float speed, tilt;
+	private Vector3 target = new Vector3 (0, 1.5f, 0);
+
+	void Update () 
+	{
+		transform.position = Vector3.MoveTowards (transform.position, target, Time.deltaTime * speed);
+		if (transform.position == target && target.y != 0.5f)
+			target.y = 0.5f;
+		else if (transform.position == target && target.y == 0.5f)
+			target.y = 1.5f;
+
+		transform.Rotate (Vector3.up * tilt);
+	}
+}

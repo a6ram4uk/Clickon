e4e26fc9a290cbd9011090fc301656dba31d467c
a6ram4uk <a6ram4uk@mail.ru>
2016-07-08 15:12:19 +0300

    New functions for main scene's buttons

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour 
+{
+	
+	public Sprite layer_orange, layer_red;
+
+
+	void OnMouseDown ()
+	{
+		GetComponent <SpriteRenderer> ().sprite = layer_red;
+	}
+
+	void OnMouseUp ()
+	{
+		GetComponent <SpriteRenderer> ().sprite = layer_orange;
+	}
+
+	void OnMouseUpAsButton () 
+	{
+		switch (gameObject.name) 
+		{
+		case "Play":
+			Application.LoadLevel ("play");
+			break;
+		case "Rating":
+			Application.OpenURL ("http://google.com");
+			break;
+		}
+	}
+}
